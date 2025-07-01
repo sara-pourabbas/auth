e21ebd72a588425e861276a2830867802154668c
s.pourabbas <sara.poorabas@gmail.com>
2025-07-01 10:15:23 +0330

    clean code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
+import { Vazirmatn } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const vazir = Vazirmatn({
+  subsets: ['arabic'],
+  weight: ['400', '700'],
+  display: 'swap',
+}); 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="fa" dir="rtl" className={vazir.className}>
+      <body>
         {children}
       </body>
     </html>
